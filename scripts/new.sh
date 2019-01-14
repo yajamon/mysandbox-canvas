69467b142b5654b1df61969839035afa13aa9f4f
yajamon <yajamon.tatsuki@gmail.com>
2019-01-15 00:52:09 +0900

    生成スクリプト追加
    それに伴いsquareのスクリプトを修正
    本当はmain関数も用意するつもりはなかったんだけど、個別のtsファイル内で同じ名称を使えなかった
    多分tsconfig.jsonのstrictあたりの影響で実際には重複しないにもかかわらず怒られている

---
+++ scripts/new.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+readonly ROOT=$(cd $(dirname $BASH_SOURCE)/..; pwd)
+readonly SRC_ROOT="$ROOT/src"
+
+if [ -z $1 ]; then
+    echo "require name." >&2
+    exit 1;
+fi
+
+cat << EOT > "${SRC_ROOT}/${1}.html"
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${1}</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+
+<body>
+    <canvas id="canvas" width="640" height="360"></canvas>
+    <script src="./script/${1}.js"></script>
+</body>
+
+</html>
+EOT
+
+cat << EOT > "${SRC_ROOT}/script/${1}.ts"
+function ${1//-/_}_main() {
+    const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d")!;
+}
+${1//-/_}_main();
+EOT
--- src/script/square.ts
+++ src/script/square.ts
@@ -1,7 +1,10 @@
-const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
-const ctx = canvas.getContext("2d")!;
+function square_main() {
+    const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d")!;
 
-ctx.fillRect(0, 0, 50, 50);
+    ctx.fillRect(0, 0, 50, 50);
 
-ctx.fillStyle = "green";
-ctx.fillRect(50, 50, 50, 50);
+    ctx.fillStyle = "green";
+    ctx.fillRect(50, 50, 50, 50);
+}
+square_main();
