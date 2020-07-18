7e2584eb143e46ff72937f1dbf0896963d6887a9
yajamon <yajamon.tatsuki@gmail.com>
2020-07-18 17:40:19 +0900

    Stateを追加してみる

--- src/script/moving-ball.ts
+++ src/script/moving-ball.ts
@@ -5,10 +5,21 @@ const ctx = canvas.getContext("2d")!;
 const msForCycle = 1000 / 60;
 const [w, h] = [canvas.width, canvas.height];
 
-const render = (ctx: CanvasRenderingContext2D) => {
+type State = {
+}
+
+const update = (state: State): State => {
+    const newState = { ...state };
+    return newState
+};
+
+const render = (ctx: CanvasRenderingContext2D, state: State) => {
     ctx.clearRect(0, 0, w, h);
 };
 
+let state: State = {
+}
 setInterval(() => {
-    render(ctx);
+    state = update(state);
+    render(ctx, state);
 }, msForCycle);
